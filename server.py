37952788a31979edc51b1e4439dc7ada85cfb0b2
Night2077 <matheus.a.brito12@gmail.com>
2025-12-04 03:51:56 -0300

    T1.3

--- server.py
+++ server.py
@@ -1,5 +1,62 @@
+import hashlib
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask import request, make_response, jsonify, redirect, url_for, abort
-from database_test import app, db, User #, Session
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidKey
+from base64 import b64encode, b64decode
+import os
+
+app = Flask(__name__)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASE_DIR, "users.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+# Modelo de Usuário
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    nome = db.Column(db.String(120), nullable=False)
+    senha = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+# Definição do Modelo de Sessão 1.3
+class Session(db.Model):
+    session_id = db.Column(db.String(32), primary_key=True) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
+
+#Derivando o digest da senha e codificando em Base64
+def gera_digest_senha(password):
+
+    salt = os.urandom(16)
+    kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1, backend=default_backend()) 
+    digest = kdf.derive(password.encode("utf-8")) # derivar o digest da senha
+    combinado = salt + digest  
+    bash_base64_em_str = b64encode(combinado).decode('ascii') # codificar em Base64
+
+    return bash_base64_em_str
+
+#Verificação da senha
+def verifica_senha(password, senha_hash_base64):
+
+    combinado = b64decode(senha_hash_base64.encode("ascii")) # decodificar de Base64 para bytes
+    salt = combinado[:16]
+    digest_real = combinado[16:]
+    kdf = Scrypt(salt =salt, length =32, n=2**14, r=8, p=1, backend=default_backend())
+    try:
+        kdf.verify(password.encode("utf-8"), digest_real) # verificar a senha
+        return True
+    except InvalidKey:
+        return False
+    
+#Geração do ID da Sessão
+def gerar_session_id():
+    random_bytes = os.urandom(16)
+    return hashlib.md5(random_bytes).hexdigest()
 
 #1.1 Cadastro de Usuário
 @app.route("/cadastro", methods=["GET", "POST"])
@@ -37,22 +94,15 @@ def cadastro():
         # Log dos dados recebidos
         print(f"Dados recebidos - Email: {email}, Nome: {nome}, Senha: {senha}")
 
-        # Validação básica dos campos
-        if not all([email, nome, senha]):
-            return make_response(jsonify({
-                "erro": "Todos os campos (email, nome, senha) são obrigatórios"
-            }), 400)
-
         # Verificar se email já existe
         usuario_existente = User.query.filter_by(email=email).first()
         if usuario_existente:
-            return make_response(jsonify({
-                "erro": "Email já cadastrado"
-            }), 409)
+            return make_response("erro Email já cadastrado", 409)
 
         else:
             # Criação do usuário
-            novo_usuario = User(email=email, nome=nome, senha=senha)
+            senha_hash = gera_digest_senha(senha) # hash da senha
+            novo_usuario = User(email=email, nome=nome, senha=senha_hash)
             db.session.add(novo_usuario)
             db.session.commit()
 
@@ -62,9 +112,8 @@ def cadastro():
         return resposta
 
 #1.2 Login de Usuário
-
 @app.route('/login', methods=['GET', 'POST'])
-def fake_login():
+def login():
     if request.method == 'GET': # mostrar formulário de login
         return """
         <h2>Login</h2>
@@ -78,16 +127,28 @@ def fake_login():
     if request.method == "POST":
         email = request.form.get("email")
         senha = request.form.get("senha")
-        user = User.query.filter_by(email=email, senha=senha).first() # buscar usuário no banco de dados
+        
+        user = User.query.filter_by(email=email).first()
 
-        if not user: 
+        if not user:
             return abort(401, description="Credenciais inválidas")
+        
+        if not verifica_senha(senha, user.senha):
+            return abort(401, description="Credenciais inválidas")
+        
+        # Criar nova sessão
+        session_id = gerar_session_id()
+        sess = Session(session_id=session_id, user_id=user.id)
+        db.session.add(sess)
+        db.session.commit()
 
         # login bem sucedido 
         resp = make_response("Login OK!", 200)
-        resp.set_cookie("user_id", str(user.id))  # setar cookie com ID do usuário
-
+        resp.set_cookie("session_id", session_id)  # setar cookie com ID da sessão
+        
+        #contenty_type
         print(f"content_type: {request.content_type}")
+        print(f"session cookies: {session_id}")
         print(f"Email recebido: {email}")
         print(f"Senha recebida: {senha}")
 
@@ -95,6 +156,7 @@ def fake_login():
 
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
+
     if request.method == "GET": # mostrar formulário de logout
         return """
         <h2>Logout</h2>
@@ -104,48 +166,55 @@ def logout():
         """, 200
 
     if request.method == "POST":
-        resp = make_response("Logout realizado", 200)
-        resp.set_cookie("user_id", "X", max_age=0)   # apaga cookie
-        return resp
+        
+        session_id = request.cookies.get("session_id")
+        # Remover sessão do banco de dados
+        if session_id:
+            sess = Session.query.filter_by(session_id=session_id).first()
+            if sess:
+                db.session.delete(sess)
+                db.session.commit()
 
-#@app.route('/delete-cookie')
-#def delete_cookie():
-#    res.set_cookie('user', 'qualquer_coisa', max_age=0)
-#    return res 
+        resp = make_response("Logout realizado", 200)
+        resp.set_cookie("session_id", "X", max_age=0)   # apaga cookie
+        return resp
 
 # Página inicial
 @app.route("/")
 def home():
-    user_id = request.cookies.get("user_id")
-    if not user_id or user_id == "X": # Redireciona para login se não estiver autenticado
+
+    session_id = request.cookies.get("session_id")
+
+    if not session_id:
         return """
-        <html>
-        <head><title>Portal Principal</title></head>
-        <body>
-            <h1>Bem-vindo ao Portal Principal</h1>
-            <p>Você não está logado.</p>
-            <a href="/login"><button>Fazer Login</button></a>
-            <a href="/cadastro"><button>Cadastrar</button></a>
-        </body>
-        </html>
+        <h1>Bem-vindo ao Portal Principal</h1>
+        <p>Você não está logado.</p>
+        <a href="/login"><button>Fazer Login</button></a>
+        <a href="/cadastro"><button>Cadastrar</button></a>
         """, 200
     
-    user = User.query.get(user_id) # buscar usuário no banco de dados
-    if not user: # Cookie inválido - redireciona para login
+    # Busca sessão no banco de dados
+    sess = Session.query.filter_by(session_id=session_id).first()
+    if not sess:
+        return """
+        <h1>Bem-vindo ao Portal Principal</h1>
+        <p>Você não está logado.</p>
+        <a href="/login"><button>Fazer Login</button></a>
+        <a href="/cadastro"><button>Cadastrar</button></a>
+        """, 200
+
+    # Busca usuário da sessão
+    user = User.query.get(sess.user_id)
+    if not user:
         resp = make_response(redirect(url_for('login')))
-        resp.set_cookie("user_id", "", max_age=0)
+        resp.set_cookie("session_id", "", max_age=0)
         return resp
 
-    # Usuário autenticado - mostrar página principal
+    # página autenticada
     return f"""
-    <html>
-    <head><title>Portal Principal</title></head>
-    <body>
-        <h1>Bem-vindo, {user.nome}!</h1>
-        <p>Email: {user.email}</p>
-        <a href="/logout"><button>Sair</button></a>
-    </body>
-    </html>
+    <h1>Bem-vindo, {user.nome}!</h1>
+    <p>Email: {user.email}</p>
+    <a href="/logout"><button>Sair</button></a>
     """, 200
 
 
---
+++ test.py
@@ -0,0 +1,44 @@
+#from hmac import digest
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import request, make_response, jsonify, redirect, url_for, abort
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+from cryptography.hazmat.backends import default_backend
+from cryptography.exceptions import InvalidKey
+from base64 import b64encode, b64decode
+import os
+
+def gera_digest_senha(password):
+
+    salt = os.urandom(16)
+    kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1, backend=default_backend())
+     
+    digest = kdf.derive(password.encode("utf-8"))
+    
+    combinado = salt + digest  
+    
+    bash_base64_em_str = b64encode(combinado).decode('ascii')
+    hash_em_bytes_denovo = b64decode(bash_base64_em_str.encode('ascii'))
+    #return b64encode(combinado).decode("ascii")
+    return bash_base64_em_str,hash_em_bytes_denovo
+
+#Verificação da senha
+def verifica_senha(password, senha_hash_base64):
+
+    combinado = b64decode(senha_hash_base64.encode("ascii"))
+    salt = combinado[:16]
+    digest_real = combinado[16:]
+    
+    kdf = Scrypt(salt =salt, length =32, n=2**14, r=8, p=1, backend=default_backend())
+    
+    try:
+        kdf.verify(password.encode("utf-8"), digest_real)
+        return True
+    except InvalidKey:
+        return False
+
+
+
+#print(gera_digest_senha("minha_senha_secreta"))
+
+print(verifica_senha("minha_senha_secreta",gera_digest_senha("minha_senha_secreta")[0]))
